405ced5168616f91599cce75625ff4283ea4f427
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-01-14 01:47:19 +0000

    implement cors filter

---
+++ src/main/java/pt/ipleiria/estg/ei/dae_proj/embalagens_inteligentes_dae/providers/CorsFilter.java
@@ -0,0 +1,23 @@
+package pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.providers;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "*");
+    }
+}
