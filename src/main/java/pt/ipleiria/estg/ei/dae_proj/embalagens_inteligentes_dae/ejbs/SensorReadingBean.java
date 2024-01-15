b873768a7610520e6299587dcd6d59537b2b16bc
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-01-15 19:58:28 +0000

    verify quality constraints on sensor reading acquisition

--- src/main/java/pt/ipleiria/estg/ei/dae_proj/embalagens_inteligentes_dae/ejbs/SensorReadingBean.java
+++ src/main/java/pt/ipleiria/estg/ei/dae_proj/embalagens_inteligentes_dae/ejbs/SensorReadingBean.java
@@ -2,10 +2,12 @@ package pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.ejbs;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.comparators.SensorReadingComparator;
 import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.entities.Sensor;
 import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.entities.SensorReading;
 import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.exceptions.MyEntityExistsException;
 import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.exceptions.MyEntityNotFoundException;
+import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.utils.QualityConstraintsVerifier;
 
 import java.util.List;
 
@@ -18,8 +20,22 @@ public class SensorReadingBean {
         return entityManager.find(SensorReading.class, id) != null;
     }
 
-    public SensorReading create(float value) throws MyEntityExistsException {
-        var sensorReading = new SensorReading(value);
+    public SensorReading create(float value, long sensor_id) throws MyEntityNotFoundException {
+        // find the sensor by ID
+        Sensor s = entityManager.find(Sensor.class, sensor_id);
+        if(s == null)
+            throw new MyEntityNotFoundException("Specified tensor does not exist!");
+
+        SensorReading sensorReading = new SensorReading(value, s);
+
+        // check the constraints this sensor controls
+        try {
+            sensorReading.setViolatesQualityConstraint(!QualityConstraintsVerifier.verifyConstraintListOnReading(
+                    s.getControlledConstraints(), sensorReading));
+        } catch (Exception e) {
+            System.err.println("Given constraint was invalid!");
+        }
+
         entityManager.persist(sensorReading);
         return sensorReading;
     }
@@ -36,23 +52,4 @@ public class SensorReadingBean {
             throw new MyEntityNotFoundException("Sensor Reading with id: " + id + " not found");
         return sensorReading;
     }
-
-    public void update(float value, long id) {
-        SensorReading sensorReading = entityManager.find(SensorReading.class, id);
-        if (sensorReading != null) {
-            sensorReading.setValue(value);
-            entityManager.merge(sensorReading);
-        }
-    }
-
-    public void delete(long id) throws MyEntityNotFoundException {
-
-        SensorReading sensorReading = entityManager.find(SensorReading.class, id);
-        if (sensorReading == null)
-            throw new MyEntityNotFoundException("Sensor with id: " + id + " not found");
-
-        //entityMannager.lock(order, LockModeType.OPTIMISTIC); ???????????
-        entityManager.remove(sensorReading);
-    }
-
 }
