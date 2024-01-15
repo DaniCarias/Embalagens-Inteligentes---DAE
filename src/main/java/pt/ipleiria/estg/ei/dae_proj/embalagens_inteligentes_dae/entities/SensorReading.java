1c413e50007240bd52a90f61d96a0d4c973eb994
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-01-15 20:03:02 +0000

    associate a sensor to a reading. flag quality constraint violations

--- src/main/java/pt/ipleiria/estg/ei/dae_proj/embalagens_inteligentes_dae/entities/SensorReading.java
+++ src/main/java/pt/ipleiria/estg/ei/dae_proj/embalagens_inteligentes_dae/entities/SensorReading.java
@@ -6,10 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import java.util.Date;
 
 @Entity
-@Table(
-        name="sensor_readings",
-        uniqueConstraints = @UniqueConstraint(columnNames = {"id"})
-)
+@Table(name="sensor_readings")
 public class SensorReading {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,21 +20,42 @@ public class SensorReading {
     @OneToOne
     private Sensor sensor;
 
+    boolean violatesQualityConstraint = false;
+
     public SensorReading(){
     }
 
-    public SensorReading(float value) {
+    public SensorReading(float value, Sensor sensor) {
         this.date = new Date();
         this.value = value;
+        this.sensor = sensor;
     }
 
     public Date getDate() {
         return date;
     }
+
     public float getValue() {
         return value;
     }
+
+    public Sensor getSensor() {
+        return this.sensor;
+    }
+
+    public boolean doesViolateQualityConstraint() {
+        return violatesQualityConstraint;
+    }
+
     public void setValue(float value) {
         this.value = value;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    public void setViolatesQualityConstraint(boolean violates) {
+        this.violatesQualityConstraint = violates;
     }
 }
