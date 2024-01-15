2bc4bf88221bf65ce5d508a4997c729ab02b96d0
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-01-15 22:47:01 +0000

    get violating sensor readings for package and product

--- src/main/java/pt/ipleiria/estg/ei/dae_proj/embalagens_inteligentes_dae/ejbs/SensorReadingBean.java
+++ src/main/java/pt/ipleiria/estg/ei/dae_proj/embalagens_inteligentes_dae/ejbs/SensorReadingBean.java
@@ -1,14 +1,21 @@
 package pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.ejbs;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.PersistenceContext;
+import org.hibernate.Hibernate;
+import org.hibernate.jpa.internal.HintsCollector;
 import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.comparators.SensorReadingComparator;
+import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.entities.Product;
+import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.entities.QualityConstraint;
 import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.entities.Sensor;
 import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.entities.SensorReading;
+import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.entities.Package;
 import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.exceptions.MyEntityExistsException;
 import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.exceptions.MyEntityNotFoundException;
 import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.utils.QualityConstraintsVerifier;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class SensorReadingBean {
@@ -50,5 +57,63 @@ public class SensorReadingBean {
         if (sensorReading == null)
             throw new MyEntityNotFoundException("Sensor Reading with id: " + id + " not found");
         return sensorReading;
+    }
+
+    public List<SensorReading> getViolatingSensorReadingsForProduct(long product_id) throws MyEntityNotFoundException {
+        // TODO: probably this is not how it should be done. should be done with queries
+        // get product by id
+        Product product = entityManager.find(Product.class, product_id);
+        if(product == null)
+            throw new MyEntityNotFoundException("Product does not exist!");
+
+        List<SensorReading> readings = new LinkedList<>();
+
+        // initialize constraints list
+        Hibernate.initialize(product.getQualityConstraints());
+
+        // iterate product constraints
+        for(QualityConstraint c : product.getQualityConstraints()) {
+
+            Hibernate.initialize(c.getSensor());
+            Hibernate.initialize(c.getSensor().getReadings());
+
+            // iterating readings og the sensor monitoring the constraint
+            for(SensorReading r : c.getSensor().getReadings()) {
+                // add violating constraints
+                if(r.doesViolateQualityConstraint())
+                    readings.add(r);
+            }
+        }
+
+        return readings;
+    }
+
+    public List<SensorReading> getViolatingSensorReadingsForPackage(long package_id) {
+        // TODO: probably this is not how it should be done. should be done with queries
+
+        // get package by id
+        Package _package = entityManager.find(Package.class, package_id);
+        Hibernate.initialize(_package.getProduct());
+
+        List<SensorReading> readings = new LinkedList<>();
+
+        // initialize constraints list
+        Hibernate.initialize(_package.getProduct().getQualityConstraints());
+
+        // iterate product constraints
+        for(QualityConstraint c : _package.getProduct().getQualityConstraints()) {
+
+            Hibernate.initialize(c.getSensor());
+            Hibernate.initialize(c.getSensor().getReadings());
+
+            // iterating readings og the sensor monitoring the constraint
+            for(SensorReading r : c.getSensor().getReadings()) {
+                // add violating constraints
+                if(r.doesViolateQualityConstraint())
+                    readings.add(r);
+            }
+        }
+
+        return readings;
     }
 }
