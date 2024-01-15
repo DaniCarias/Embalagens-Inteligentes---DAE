b6b1075d06bcc70093ea815cb89e64fbf9e67c50
Daniel Carias <dfcpcarias@gmail.com>
2024-01-15 15:15:59 +0000

    Create Sensor Bean CRUD

---
+++ src/main/java/pt/ipleiria/estg/ei/dae_proj/embalagens_inteligentes_dae/ejbs/SensorBean.java
@@ -0,0 +1,56 @@
+package pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.ejbs;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.entities.EndConsumer;
+import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.entities.Sensor;
+import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.exceptions.MyEntityExistsException;
+import pt.ipleiria.estg.ei.dae_proj.embalagens_inteligentes_dae.exceptions.MyEntityNotFoundException;
+
+import java.util.List;
+
+public class SensorBean {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public boolean exists(long id) {
+        return entityManager.find(Sensor.class, id) != null;
+    }
+
+    public Sensor create(String name) throws MyEntityExistsException {
+        var sensor = new Sensor(name);
+        entityManager.persist(sensor);
+        return sensor;
+    }
+
+    public List<Sensor> getAllSensor() {
+        return entityManager.createNamedQuery("getAllSensores", Sensor.class).getResultList();
+    }
+
+    public Sensor getSensor(String name) throws MyEntityNotFoundException {
+        Sensor sensor = entityManager.find(Sensor.class, name);
+        if (sensor == null)
+            throw new MyEntityNotFoundException("Sensor with name: " + name + " not found");
+        return sensor;
+    }
+
+    public void update(String name) {
+        Sensor sensor = entityManager.find(Sensor.class, name);
+        if (sensor != null) {
+            sensor.setName(name);
+            entityManager.merge(sensor);
+        }
+    }
+
+    public void delete(String name) throws MyEntityNotFoundException {
+
+        Sensor sensor = entityManager.find(Sensor.class, name);
+        if (sensor == null)
+            throw new MyEntityNotFoundException("Sensor with name: " + name + " not found");
+
+        //entityMannager.lock(order, LockModeType.OPTIMISTIC); ???????????
+        entityManager.remove(sensor);
+    }
+
+}
