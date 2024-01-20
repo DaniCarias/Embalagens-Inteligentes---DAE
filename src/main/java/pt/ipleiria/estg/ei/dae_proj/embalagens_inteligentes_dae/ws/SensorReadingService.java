e491e08628cb306704753bc8df9b923cad243cc9
Daniel Carias <dfcpcarias@gmail.com>
2024-01-20 15:41:23 +0000

    Update roles

--- src/main/java/pt/ipleiria/estg/ei/dae_proj/embalagens_inteligentes_dae/ws/SensorReadingService.java
+++ src/main/java/pt/ipleiria/estg/ei/dae_proj/embalagens_inteligentes_dae/ws/SensorReadingService.java
@@ -24,7 +24,7 @@ import java.util.stream.Collectors;
 @Produces({MediaType.APPLICATION_JSON})
 @Consumes({MediaType.APPLICATION_JSON})
 @Authenticated
-@RolesAllowed({"LogisticOperator"})
+@RolesAllowed({"LogisticOperator", "ProductManufacturer", "EndConsumer"})
 public class SensorReadingService {
 
     @EJB
